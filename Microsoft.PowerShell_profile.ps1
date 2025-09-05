0a4a9bdb827d567a780f25ff37ee36bdb53138b7
ensolation <126297433+ensolation@users.noreply.github.com>
2025-09-04 23:53:44 -0400

    Add files via upload

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,12 @@
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
 $host.UI.RawUI.WindowTitle = ""
 Invoke-Expression (&starship init powershell)
 . "C:\Users\boibl\Downloads\Invoke-WslCommand.ps1"
@@ -6,8 +15,9 @@ function global:cmatrix {
 }
 function cava { & "C:\Users\boibl\Downloads\cava_win\cava.exe" }
 Set-Alias gt ghosttime
+Set-Alias ls eza
 Set-Alias ff fastfetch
-Set-Alias vim .\vim.exe
+Set-Alias vim hx
 Import-Module "C:\Users\boibl\winwal\winwal.psm1"
 Enable-PowerType
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView
