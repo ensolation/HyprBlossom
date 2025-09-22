215adde975468576210aee9017a8616f3df167e7
ensolation <126297433+ensolation@users.noreply.github.com>
2025-09-22 00:17:50 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
+cd
 function y {
     $tmp = (New-TemporaryFile).FullName
     yazi $args --cwd-file="$tmp"
@@ -7,28 +8,19 @@ function y {
     }
     Remove-Item -Path $tmp
 }
-$host.UI.RawUI.WindowTitle = ""
 Invoke-Expression (&starship init powershell)
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+Import-Module PoshColor
 . "C:\Users\boibl\Downloads\Invoke-WslCommand.ps1"
 function global:cmatrix {
 	Invoke-WslCommand -Command "cmatrix" -Arguments $args
 }
 function cava { & "C:\Users\boibl\Downloads\cava_win\cava.exe" }
-Set-Alias gt ghosttime
+Set-Alias ts .\theme.ps1
 Set-Alias ls eza
 Set-Alias ff fastfetch
 Set-Alias vim hx
+Set-Alias nf neofetch
 Import-Module "C:\Users\boibl\winwal\winwal.psm1"
 Enable-PowerType
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView
-
-
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle InlineView
